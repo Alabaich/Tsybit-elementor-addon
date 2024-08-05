bc1368befe4a90e3f33782d4a712daab32f743cb
ReddiJe <nikitosciba@gmail.com>
2024-08-06 00:05:09 +0200

    All Services Styles

--- widgets/allServices.php
+++ widgets/allServices.php
@@ -170,8 +170,16 @@ class allServices extends \Elementor\Widget_Base
                 border: 1px solid #2c2d2c;
             }
 
-            .allServices .textSide .link:hover svg {
-                border: #2c2d2c;
+            .allServices .textSide .icon {
+                transition: stroke 300ms linear;
+            }
+
+            .allServices .textSide a:hover .icon path {
+                stroke: #2C2D2C;
+            }
+
+            .allServices .textSide .icon path {
+                stroke: #F0F0F3;
             }
 
             .allServices .allServicesBlocks {
@@ -217,9 +225,13 @@ class allServices extends \Elementor\Widget_Base
 
                 <?php if (!empty($settings['buttonText'])) : ?>
                     <a class="link" href="<?php echo $settings['buttonUrl']['url']; ?>">
-                        <?php echo $settings['buttonText']; ?> <svg width="11" height="11" viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M9.74253 1.18198H3.37857M9.74253 1.18198V7.54594M9.74253 1.18198L4.17406 6.75045M1.25725 9.66726L2.58307 8.34144" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                        </svg>
+                        <?php echo $settings['buttonText']; ?> <a class="link" href="<?php echo $settings['buttonUrl']['url']; ?>">
+                            <?php echo $settings['buttonText']; ?>
+                            <svg class="icon" width="11" height="11" viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M9.74253 1.18198H3.37857M9.74253 1.18198V7.54594M9.74253 1.18198L4.17406 6.75045M1.25725 9.66726L2.58307 8.34144" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                        </a>
+
 
                     </a>
                 <?php endif; ?>
