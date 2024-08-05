b2b776aaecec8348d96b3d18802fc6e4035d57ad
ReddiJe <nikitosciba@gmail.com>
2024-08-05 16:43:46 +0200

    123

--- widgets/richText.php
+++ widgets/richText.php
@@ -144,6 +144,9 @@ class richText extends \Elementor\Widget_Base
             @media screen and (max-width: 600px) {
                 .richText .richTextCentered {
                     padding: 0px;
+                }
+
+                .richText .richTextCentered {
                     margin: 50px 0px 50px 0px;
                 }
 
