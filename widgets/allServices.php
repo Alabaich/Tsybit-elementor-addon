bcc260bac2ad244a89d4e05d87f40123f3d4ce08
ReddiJe <nikitosciba@gmail.com>
2024-08-05 23:47:46 +0200

    All Services

--- widgets/allServices.php
+++ widgets/allServices.php
@@ -172,7 +172,7 @@ class allServices extends \Elementor\Widget_Base
                 flex-wrap: wrap;
                 justify-content: center;
                 gap: 25px;
-                margin-top: 20px;
+                margin: 20px 76px 0px;
             }
 
             .allServices .allServicesBlocks .oneServiceBlock {
